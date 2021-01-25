728f47a76943f2f7ec5a89aa66b5fb96c8e0c3af
paulspartan14 <e17081416@itmerida.edu.mx>
2021-01-25 02:59:14 -0600

    added documentation swagger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 const routerStudents = require('./students')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../swagger.json')
+
+
+router.use('/docs', swaggerUi.serve)
+router.get('/docs', swaggerUi.setup(swaggerDocument))
 
 router.get('/', (req, res) => {
   res.send({ message: 'Welcome Home!' })
