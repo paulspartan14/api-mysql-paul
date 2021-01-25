1370c99cde64c0148b13b7ae76d2737cac96b58b
paulspartan14 <e17081416@itmerida.edu.mx>
2021-01-25 02:15:26 -0600

    refactorized code with async await

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,4 +1,6 @@
-const dataModels = require('../models/students')
+const util = require('util')
+const db_connection = require('../database/connection')
+const query = util.promisify(db_connection.query).bind(db_connection)
 
 /**
  * @function getStudents
@@ -6,82 +8,93 @@ const dataModels = require('../models/students')
  * @param {Object} res
  */
 
-const getStudents = (req, res) => {
-  dataModels.getStudents((data, err) => {
-    return res.status(200).send({
-      data,
-    })
-  })
+const getStudents = async (req, res) => {
+  try {
+    let sql = `select * from students`
+    const rows = await query(sql)
+    res.status(200).json(rows)
+  } catch (err) {
+    console.error(`there was an error: ${err}`)
+  }
 }
 
 /**
  * @function getStudentById
  * @description Controller for GET with StudentID /api/v1/students/:id
- * @param {Object} req 
- * @param {Object} res 
- * @param {Function} next 
+ * @param {Object} req
+ * @param {Object} res
  */
 
-const getStudentById = (req, res) => {
+const getStudentById = async (req, res) => {
   const { id } = req.params
-  dataModels.getStudentById(id, (data, err) => {
-    return res.status(200).send({
-      data
-    })
-  })
+  try {
+    let sql = `select * from students where id = ${db_connection.escape(id)}`
+    const rows = await query(sql)
+    // if ID not exist...
+    if (rows.length === 0) {
+      res.status(200).json({ message: 'ID not exist' })
+    } else {
+      res.status(200).json(rows)
+    }
+  } catch (err) {
+    console.error(`there was an error: ${err}`)
+  }
 }
 
 /**
  * @function postStudent
  * @description Controller for POST student /api/v1/students/
- * @param {Object} req 
- * @param {Object} res 
- * @param {Function} next 
+ * @param {Object} req
+ * @param {Object} res
  */
 
-const postStudent = (req, res) => {
-  const {name, lastname} = req.body
-  dataModels.postStudent({name, lastname}, (data, err) =>{
-    return res.status(201).send({
-      data
-    })
-  })
+const postStudent = async (req, res) => {
+  const { name, lastname } = req.body
+  try {
+    let sql = `insert into students(name, lastname) values (${db_connection.escape(name)}, ${db_connection.escape(lastname)})`
+    const rows = await query(sql)
+    res.status(201).json({ message: 'Added student successfull' })
+  } catch (err) {
+    console.error(`there was an error: ${err}`)
+  }
 }
 
 /**
  * @function putStudentById
  * @description Controller for PUT with StudentID /api/v1/students/:id
- * @param {Object} req 
- * @param {Object} res 
- * @param {Function} next 
+ * @param {Object} req
+ * @param {Object} res
  */
 
-const putStudentById = (req, res) => {
+const putStudentById = async (req, res) => {
   const { id } = req.params
-  const { name, lastname} = req.body
-  dataModels.putStudentById({ id, name, lastname }, (data, err) =>{
-    return res.status(201).send({
-      data
-    })
-  })
-  
+  const {name, lastname} = req.body
+  try{
+    let sql = `update students set name = ${db_connection.escape(name)}, lastname = ${db_connection.escape(lastname)} where id = ${db_connection.escape(id)}`
+    const rows = await query(sql)
+    res.status(201).json({ message: 'Updated student successfull' })
+  }catch (err) {
+    console.error(`there was an error: ${err}`)
+  }
 }
 
 /**
  * @function deleteStudentById
  * @description Controller for DELETE with StudentID /api/v1/students/:id
- * @param {Object} req 
- * @param {Object} res 
- * @param {Function} next 
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
  */
 
-const deleteStudentById = (req, res) => {
+const deleteStudentById = async (req, res) => {
   const { id } = req.params
-  dataModels.deleteStudentById(id, (data, err) => {
-    return res.status(200).send({
-      data
-    })
-  })
+  try{
+    let sql = `delete from students where id = ${db_connection.escape(id)}`
+    const rows = await query(sql)
+    res.status(200).json({ message: 'Delete student successfull' })
+  }catch(err){
+    console.error(`there was an error: ${err}`)
+  }
 }
 
 module.exports = {
@@ -89,5 +102,5 @@ module.exports = {
   getStudentById,
   postStudent,
   putStudentById,
-  deleteStudentById,
+  deleteStudentById
 }
